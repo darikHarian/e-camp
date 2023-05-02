332d55118a8ff45c32b28288753aabbe6b0426a8
Darik Harian Khal <contacto@alphacorp.cl>
2023-05-02 10:34:39 -0400

    modulo 2 - Sesion 11 - Ejercicio de comprobacion

---
+++ fullStackJavaScript_0021/Modulo2/Sesion11/ejercicioDeComprobacion/index.js
@@ -0,0 +1,20 @@
+let card = document.querySelector('button')
+// let newJoke = document.querySelector('#cardText')
+
+card.addEventListener('click', () => {
+
+    fetch('https://geek-jokes.sameerkumar.website/api?format=json')
+        .then(response => response.json())
+        .then(info => {
+            card(info)
+        })
+
+    function card(info) {
+        cardContent.innerHTML = ""
+        cardContent.innerHTML += `
+            <p class="fs-4">
+                ${info.joke}
+            </p>
+        `
+    }  
+})
