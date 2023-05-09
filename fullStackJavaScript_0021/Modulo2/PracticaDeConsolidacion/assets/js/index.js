5f85a1c0c2f1dcb82c6803055f567d6e0b9f62a2
Darik Harian Khal <contacto@alphacorp.cl>
2023-05-09 16:17:08 -0400

    Modulo 2 - Practica de consolidacion - Agregando funcionalidad con JS v.8

--- fullStackJavaScript_0021/Modulo2/PracticaDeConsolidacion/assets/js/index.js
+++ fullStackJavaScript_0021/Modulo2/PracticaDeConsolidacion/assets/js/index.js
@@ -14,14 +14,14 @@ digiSearch.addEventListener('click', (event) => {
         document.querySelector('#nombre').setAttribute("class", "d-block text-center text-white text-shadow-black drop-shadow-black rounded-pill mt-3 py-1 mx-auto")
         nombre.innerHTML = ''
         nombre.innerHTML = `
-                <h3 class="pt-1">Nombre: ${info[0].name}</h3>
-            `
+            <h3 class="pt-1">Nombre: ${info[0].name}</h3>
+        `
 
         document.querySelector('#nivel').setAttribute("class", "d-block text-center text-white text-shadow-black drop-shadow-black rounded-pill mt-2 py-1 mx-auto")
         nivel.innerHTML = ''
         nivel.innerHTML = `
-                <h3 class="pt-1">Nivel: ${info[0].level}</h3>
-            `
+            <h3 class="pt-1">Nivel: ${info[0].level}</h3>
+        `
     })
 })
 
@@ -29,12 +29,41 @@ window.onload = function() {
     fetch('https://digimon-api.vercel.app/api/digimon')
     .then(response => response.json())
     .then(list => {
+        i = 1
         for (let digimon of list) {
             digiThumb.innerHTML += `
                 <li>
-                    <a href="#search" class="btn listButton text-white text-shadow-black drop-shadow-black rounded-pill border-0 fs-1 my-1">${digimon.name} <img class="rounded-circle mb-1" style="width:30px" src="${digimon.img}"></a>
+                    <a id="${digimon.name}" href="#search" class="btn listButton text-white text-shadow-black drop-shadow-black rounded-pill border-0 fs-1 my-1">${digimon.name} <img class="rounded-circle mb-1" style="width:30px" src="${digimon.img}"></a>
                 </li>
             ` 
         }       
     })
 }
+
+let digiList = document.querySelector("#digiThumb")
+
+digiList.addEventListener("click", digimon => {
+    if (digimon.target && digimon.target.nodeName === "A") {
+        let digimonSelected = digimon.target.id
+        
+        fetch(`https://digimon-api.vercel.app/api/digimon/name/${digimonSelected}`)
+        .then(response => response.json())
+        .then(info => {
+            digiCard.innerHTML = ''
+            digiCard.innerHTML = `
+                <img src="${info[0].img}" alt="" class="card-img rounded-circle drop-shadow-black mx-auto p-5">
+            `
+            document.querySelector('#nombre').setAttribute("class", "d-block text-center text-white text-shadow-black drop-shadow-black rounded-pill mt-3 py-1 mx-auto")
+            nombre.innerHTML = ''
+            nombre.innerHTML = `
+                <h3 class="pt-1">Nombre: ${info[0].name}</h3>
+            `
+
+            document.querySelector('#nivel').setAttribute("class", "d-block text-center text-white text-shadow-black drop-shadow-black rounded-pill mt-2 py-1 mx-auto")
+            nivel.innerHTML = ''
+            nivel.innerHTML = `
+                <h3 class="pt-1">Nivel: ${info[0].level}</h3>
+            `
+        })
+    }
+})
