5337ee96e26b607151993c75671e305b38bfd8ff
Darik Harian Khal <contacto@alphacorp.cl>
2023-05-09 22:34:18 -0400

    Modulo 2 - Practica de consolidacion - Aplicando estilos v.11

--- fullStackJavaScript_0021/Modulo2/PracticaDeConsolidacion/assets/js/index.js
+++ fullStackJavaScript_0021/Modulo2/PracticaDeConsolidacion/assets/js/index.js
@@ -11,13 +11,13 @@ digiSearch.addEventListener('click', (event) => {
         digiCard.innerHTML = `
             <img src="${info[0].img}" alt="" class="card-img rounded-circle drop-shadow-black mx-auto p-5">
         `
-        document.querySelector('#nombre').setAttribute("class", "d-block text-center text-white text-shadow-black drop-shadow-black mt-3 py-1 mx-auto")
+        document.querySelector('#nombre').setAttribute("class", "d-block text-center text-white text-shadow-black drop-shadow-black rounded-pill mt-3 py-1 mx-auto")
         nombre.innerHTML = ''
         nombre.innerHTML = `
             Nombre: ${info[0].name}
         `
 
-        document.querySelector('#nivel').setAttribute("class", "d-block text-center text-white text-shadow-black drop-shadow-black mt-2 py-1 mx-auto")
+        document.querySelector('#nivel').setAttribute("class", "d-block text-center text-white text-shadow-black drop-shadow-black rounded-pill mt-2 py-1 mx-auto")
         nivel.innerHTML = ''
         nivel.innerHTML = `
             Nivel: ${info[0].level}
@@ -33,7 +33,7 @@ window.onload = function() {
         for (let digimon of list) {
             digiThumb.innerHTML += `
                 <li class="text-shadow-black drop-shadow-black text-center rounded-pill border-0 fs-1 my-1 mx-auto">
-                    <a id="${digimon.name}" class="btn listButton text-white" href="#nav-logo">${digimon.name} <img class="rounded-circle" style="width:30px" src="${digimon.img}"></a>
+                    <a id="${digimon.name}" class="btn listButton text-white" href="#top">${digimon.name} <img class="rounded-circle" style="width:30px" src="${digimon.img}"></a>
                 </li>
             ` 
         }       
