2d3c31d5e0edbdf80aa91c78b1570e9b4953eb7b
Darik Harian Khal <contacto@alphacorp.cl>
2023-05-15 15:28:44 -0400

    Modulo 3 - Practica de consolidacion - Agregando funcionalidad con JavaScript v.5

--- fullStackJavaScript_0021/Modulo3/PracticaDeConsolidacion/assets/js/index.js
+++ fullStackJavaScript_0021/Modulo3/PracticaDeConsolidacion/assets/js/index.js
@@ -1,13 +1,13 @@
-let ingresoDinero = document.querySelector('#botonPresupuesto')                                     // Selecciono la id #botonPresupuesto
-let ingresogastos = document.querySelector('#botonGastos')                                          // Selecciono la id #botonGastos
+let ingresoDinero = document.querySelector('#botonPresupuesto')                                     // Selecciono el botón con la id #botonPresupuesto
+let ingresogastos = document.querySelector('#botonGastos')                                          // Selecciono el botón con la id #botonGastos
 let sumaPresupuesto = 0                                                                             // Declaro la variable para acumular la suma de los ingresos de dinero
 let stringSumaPresupuesto = ''                                                                      // Declaro la variable donde se agregará el valor final de la suma, convertido a String
 let sumaGastos = 0                                                                                  // Declaro la variable para acumular la suma de los gastos
 let stringInputValorGastos = ''                                                                     // Declaro la variable donde se agregará el valor de cada gasto convertido a String
-let stringSumaGastos = ''                                                                           // Declaro la variable para acumular la suma de los gastos 
+let stringSumaGastos = ''                                                                           // Declaro la variable donde se agregará el valor final de los gastos convertido a String
 let arrGastos = []                                                                                  // Declaro la variable del arreglo en donde guardaré los objetos creados por cada gasto ingresado
 let valorTotalSaldo = 0                                                                             // Declaro la variable donde guardaré el total de presupuesto - saldo por cada actualización de estos
-let stringValorTotalSaldo = ''
+let stringValorTotalSaldo = ''                                                                      // Declaro la variable donde se agregará el valor final de la operacion de saldo convertida a String
 
 /* SECCION PRESUPUESTO */
 ingresoDinero.addEventListener('click', (noReload) => {                                             // Creo un evento click para estar a la escucha del boton de presupuesto
@@ -22,7 +22,7 @@ ingresoDinero.addEventListener('click', (noReload) => {                         
         <div class="">Presupuesto</div>
         <div class="fs-4">$${stringSumaPresupuesto}</div>
     `                                                                                               // Escribo el resultado de la suma convertida a String y con punto al resumen de presupuesto
-
+    
     totalSaldo()                                                                                    // Llamo a la funcion totalSaldo para realizar las operaciones aritméticas
     limpiarInput(inputPresupuesto)                                                                  // Llamo a la funcion limpiarInput para borrar lo escrito en el input después de hacer click en el boton Calcular
 })
@@ -36,16 +36,16 @@ ingresogastos.addEventListener('click', (noReload) => {
     let valorGastos = document.querySelector('#valorGasto')
     let gasto = new fcGastos(inputTextoGastos, inputValorGastos)                                    // Creo un nuevo objeto mediante la funcion constructora fcGastos
     arrGastos.push(gasto)                                                                           // Agrego los objetos creados al array arrGastos
-    sumaGastos += inputValorGastos
-    stringSumaGastos = sumaGastos
-    stringSumaGastos = period(stringSumaGastos)
+    sumaGastos += inputValorGastos                                                                  // Sumo a la variable el valor del input
+    stringSumaGastos = sumaGastos                                                                   // Copio el valor en otra variable para convertirlo a string mientras el original se mantiene como number para realizar operaciones
+    stringSumaGastos = period(stringSumaGastos)                                                     // Llamo a la funcion period para agregar los puntos cada 3 digitos a la cifra
     rtGastos.innerHTML = `
         <div class="">Gastos</div>
         <div class="fs-4">$${stringSumaGastos}</div>
-    `
+    `                                                                                               // Escribo el resultado de la suma de los gastos en la etiqueta con id rtgastos
     tData.innerHTML = ''                                                                            // Borro todos los elementos dentro de la etiqueta con el id tData para llenarlo con datos nuevos
-    let indice = 0                                                                          
-    arrGastos.forEach(function(gasto) {
+    let indice = 0                                                                                  // Declaro la variable indice para agregarla cuando llame a la funcion onclick
+    arrGastos.forEach(function(gasto) {                                                             // Recorro el arreglo arrGastos con forEach
         stringInputValorGastos = gasto.valor
         stringInputValorGastos = period(stringInputValorGastos)
         tData.innerHTML += `
@@ -55,7 +55,7 @@ ingresogastos.addEventListener('click', (noReload) => {
                 <td><span class="bi bi-trash-fill text-danger" onclick="eliminar(${indice})"></span></td>
             </tr>
         `
-        indice ++
+        indice ++                                                                                   // Incremento por cada iteración el valor del indice en 1
     })
 
     totalSaldo()                                                                                    // Llamo a la funcion totalSaldo para realizar las operaciones aritméticas
@@ -100,11 +100,11 @@ function eliminar(indice){
         <div class="">Gastos</div>
         <div class="fs-4">$${stringSumaGastos}</div>
     `
-    let nuevoArray = arrGastos.filter(objeto => objeto !== eliminarElemento)
-    arrGastos = nuevoArray
+    let nuevoArray = arrGastos.filter(objeto => objeto !== eliminarElemento)                        // Ocupo el método filter para generar un nuevo array sin el objeto seleccionado
+    arrGastos = nuevoArray                                                                          // Copio al array original los nuevos datos
     tData.innerHTML = ''                                                                            // Borro todos los elementos dentro de la etiqueta con el id tData para llenarlo con datos nuevos
-    let indiceElemento = 0                                                                          
-    arrGastos.forEach(function(gasto) {
+    let indiceElemento = 0                                                                          // Declaro el iterador en cero para asignarlo como argumento de la funcion onclick en el nuevo arreglo
+    arrGastos.forEach(function(gasto) {                                                             // Recorro el arreglo arrGastos con forEach
         stringInputValorGastos = gasto.valor
         stringInputValorGastos = period(stringInputValorGastos)
         tData.innerHTML += `
